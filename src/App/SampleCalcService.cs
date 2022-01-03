0c324d5121bdce69176f77eea32ce1f10e7f2dbc
Beat Walti <BWalti@users.noreply.github.com>
2022-01-03 17:17:41 +0100

    feat: added xunit
    - and some trivial first tests

---
+++ src/App/SampleCalcService.cs
@@ -0,0 +1,9 @@
+﻿namespace App;
+
+public class SampleCalcService
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
---
+++ tests/App.Tests/SampleCalcServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace App.Tests;
+public class SampleCalcServiceTests
+{
+    [Theory]
+    [InlineData(2, 3, 5)]
+    [InlineData(5, 3, 8)]
+    [InlineData(2, 2, 3)]
+    public void Adding(int a, int b, int expected)
+    {
+        var testee = new SampleCalcService();
+
+        var result = testee.Add(a, b);
+
+        Assert.Equal(expected, result);
+    }
+}
