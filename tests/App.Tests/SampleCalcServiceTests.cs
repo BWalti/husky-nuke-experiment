b7d8222f74e8d72150c34e8aad0ef9c3c5b42c59
Beat Walti <BWalti@users.noreply.github.com>
2022-01-03 17:19:49 +0100

    fix: test was wrong

--- tests/App.Tests/SampleCalcServiceTests.cs
+++ tests/App.Tests/SampleCalcServiceTests.cs
@@ -6,7 +6,7 @@ public class SampleCalcServiceTests
     [Theory]
     [InlineData(2, 3, 5)]
     [InlineData(5, 3, 8)]
-    [InlineData(2, 2, 3)]
+    [InlineData(2, 2, 4)]
     public void Adding(int a, int b, int expected)
     {
         var testee = new SampleCalcService();
